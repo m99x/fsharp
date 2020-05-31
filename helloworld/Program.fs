194cc26293278b625d8c458e28aab5d3b8ccc144
Daisuke Matsuoka <xxxmatsuoka@gmail.com>
2020-05-31 22:36:50 +0900

    commit helloworld

--- helloworld/Program.fs
+++ helloworld/Program.fs
@@ -1,8 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
+open FSharp.Scanf
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+printfn "what is the ultimate answer?"
+
+try
+  let ans = scanfn "%i"
+  if ans = 42 then
+    printfn "correct."
+  else
+    printfn "%i? no." ans
+  with
+    | _ -> printfn "you entered something other than a number."
